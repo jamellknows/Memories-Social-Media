6ed151764d0dd190b8428deb9b1934ef98fc350e
jamellknows <jamellsamuels@googlemail.com>
2021-03-22 17:16:11 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express(); /* app instance */
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+
+/*connect server application to cloud database */
+
+const CONNECTION_URL = 'mongodb+srv://Sonic100:Sonic100@cluster0.fp4wl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+//https:www.mongodb.com/cloud/atlas 
